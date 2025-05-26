dc266eeaa7603f51468e7e8d54b73b24ccbc33d4
Anirudhharanagera <105443388+Anirudhharanagera@users.noreply.github.com>
2025-05-27 00:19:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 def load_and_preprocess_data():
-    df = pd.read_csv("sample_data.csv")
+    df = pd.read_csv("sample_data_extended.csv")
     features = df.drop(columns=['CustomerID'])
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(features)
